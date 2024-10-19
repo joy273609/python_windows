7aa916595df29c4702bd9a47af49ecd9dd61528b
Joy <joy273609@gmail.com>
2024-10-19 11:48:08 +0800

    lesson4_issue2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python_windows
 ## 學院專用資料夾
-Python應用實戰_Python視窗設計_徐國堂老師
+Python應用實戰_Python視窗設計
--- lesson4/lesson4_issue/issue.py
+++ lesson4/lesson4_issue/issue.py
@@ -5,32 +5,48 @@ class Window(ThemedTk):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.title('使用ttk的套件')
-        style = ttk.Style(self)        
-        topFrame = ttk.Frame(self,borderwidth=1,relief='solid')
-        
-        btn1 = ttk.Button(topFrame,text="按鈕1")
+        style = ttk.Style(self)
+
+        style.configure('Main.TButton',font=("Arial",15,"bold"),background="lightblue",foreground="blue")
+
+        topFrame = ttk.Frame(self,borderwidth=1,relief='groove')
+        btn1 = ttk.Button(topFrame,text="按鈕1",style='Main.TButton')
         btn1.pack(side='left',expand=True,fill='x',padx=10)
-
-        btn2 = ttk.Button(topFrame,text="按鈕2")
+        btn2 = ttk.Button(topFrame,text="按鈕2",style='Main.TButton')
         btn2.pack(side='left',expand=True,fill='x')
-
-        btn3 = ttk.Button(topFrame,text="按鈕3")
+        btn3 = ttk.Button(topFrame,text="按鈕3",style='Main.TButton')
         btn3.pack(side='left',expand=True,fill='x',padx=10)
-
         topFrame.pack(padx=10,pady=(10,0),ipadx=10,ipady=10,expand=True,fill='x')
 
-        bottomFrame = ttk.Frame(self,width=500,height=300,borderwidth=1,relief='solid')
+        bottomFrame = ttk.Frame(self,height=300,borderwidth=1,relief='groove')
+        bottomFrame.pack(padx=10,pady=10)
 
-        leftFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=8,relief='solid')
+        leftFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=2,relief='groove')
+        btn1 = ttk.Button(leftFrame,text="按鈕1",style='Main.TButton')
+        btn1.pack(expand=True,fill='x',padx=10,pady=5,ipady=50)
+        btn2 = ttk.Button(leftFrame,text="按鈕2")
+        btn2.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
+        btn3 = ttk.Button(leftFrame,text="按鈕3")
+        btn3.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
         leftFrame.pack(padx=10,pady=10,side="left")
 
-        centerFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=6,relief='solid')
+        centerFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=2,relief='groove')
+        btn1 = ttk.Button(centerFrame,text="按鈕1",style='Main.TButton')
+        btn1.pack(expand=True,fill='x',padx=10,pady=5,ipady=40)
+        btn2 = ttk.Button(centerFrame,text="按鈕2")
+        btn2.pack(expand=True,fill='x',padx=10,pady=5,ipady=20)
+        btn3 = ttk.Button(centerFrame,text="按鈕3",style='Main.TButton')
+        btn3.pack(expand=True,fill='x',padx=10,pady=5,ipady=40)
         centerFrame.pack(padx=10,pady=10,side="left")
 
-        rightFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=3,relief='solid')
+        rightFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=2,relief='groove')
+        btn1 = ttk.Button(rightFrame,text="按鈕1")
+        btn1.pack(expand=True,fill='x',padx=10,pady=5,ipady=33)
+        btn2 = ttk.Button(rightFrame,text="按鈕2")
+        btn2.pack(expand=True,fill='x',padx=10,pady=5,ipady=33)
+        btn3 = ttk.Button(rightFrame,text="按鈕3")
+        btn3.pack(expand=True,fill='x',padx=10,pady=5,ipady=34)
         rightFrame.pack(padx=10,pady=10,side="right")
-
-        bottomFrame.pack(padx=10,pady=10)
 
 def main():
     window = Window(theme="arc")
